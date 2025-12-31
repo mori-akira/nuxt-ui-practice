589801fc2f7eceabb281e9568011fa5c9e4ae31c
mori-akira <kgrmrak@yahoo.co.jp>
2025-12-31 11:15:26 +0000

    update pages

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,98 @@
 <template>
-  <div>
-    <main>
+  <UApp>
+    <UHeader
+      toggle-side="left"
+      mode="slideover"
+      :ui="{
+        container: '!max-w-full',
+        toggle: '!flex',
+        title: '!hidden',
+        center: '!flex flex-1',
+        content: '!block',
+        overlay: '!block',
+      }"
+      :menu="{
+        side: 'left',
+        inset: true,
+      }"
+    >
+      <div class="flex items-center justify-center gap-2 w-full">
+        <ULink to="/" class="text-gray-800 text-2xl font-bold"
+          >Nuxt UI Practice</ULink
+        >
+      </div>
+
+      <template #right>
+        <UTooltip text="Open on GitHub">
+          <UButton
+            color="neutral"
+            variant="ghost"
+            to="https://github.com/mori-akira/nuxt-ui-practice"
+            target="_blank"
+            icon="i-simple-icons-github"
+            aria-label="GitHub"
+          />
+        </UTooltip>
+      </template>
+
+      <template #content="{ close }">
+        <div class="h-full flex flex-col">
+          <div
+            class="h-(--ui-header-height) flex items-center justify-start px-4"
+          >
+            <UButton
+              color="neutral"
+              variant="ghost"
+              icon="i-lucide-x"
+              aria-label="Close menu"
+              @click="close?.()"
+            />
+            <h2 class="text-gray-800 font-bold">メニュー</h2>
+          </div>
+
+          <div class="p-4 sm:p-6 overflow-y-auto">
+            <UNavigationMenu
+              :items="menuItems"
+              orientation="vertical"
+              class="-mx-2.5"
+            />
+          </div>
+        </div>
+      </template>
+    </UHeader>
+
+    <UMain>
       <NuxtPage />
-    </main>
-  </div>
+    </UMain>
+
+    <UFooter
+      :ui="{
+        root: 'fixed inset-x-0 bottom-0 z-50 border-t-1 border-default bg-white',
+        container: '!p-2',
+      }"
+    >
+      <template #right>
+        <p class="text-sm">Version 1.0.0</p>
+      </template>
+    </UFooter>
+  </UApp>
 </template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const route = useRoute();
+const menuItems = computed<NavigationMenuItem[]>(() => [
+  {
+    label: "ホーム",
+    to: "/",
+    active: route.path === "/",
+  },
+]);
+</script>
+
+<style scoped>
+div[data-side="left"] div[data-slot="right"] {
+  display: none;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <p class="ml-4">Nuxt UI Practice</p>
+  <UCard class="m-4">
+    <h2 class="font-bold">Top画面</h2>
+    <USeparator class="my-4" />
+    <p>以下のページを参考に、NuxtUIの学習を進める</p>
+    <ULink
+      to="https://ui.nuxt.com/"
+      class="text-blue-600 hover:text-blue-700 underline"
+      >https://ui.nuxt.com/</ULink
+    >
+  </UCard>
 </template>
